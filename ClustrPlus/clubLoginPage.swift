0b687f0effe21f4cdf2098227a67f713b290389a
Isis Decrem <60768505+isisdecrem@users.noreply.github.com>
2020-03-22 23:16:46 -0700

    added some pages that "point" to each other with buttons

---
+++ ClustrPlus/clubLoginPage.swift
@@ -0,0 +1,30 @@
+//
+//  clubLoginPage.swift
+//  ClustrPlus
+//
+//  Created by Isis Decrem on 3/22/20.
+//  Copyright © 2020 Isis Decrem. All rights reserved.
+//
+
+import UIKit
+
+class clubLoginPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ClustrPlus/memberBrowseMainscreen.swift
@@ -0,0 +1,30 @@
+//
+//  memberBrowseMainscreen.swift
+//  ClustrPlus
+//
+//  Created by Isis Decrem on 3/22/20.
+//  Copyright © 2020 Isis Decrem. All rights reserved.
+//
+
+import UIKit
+
+class memberBrowseMainscreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
