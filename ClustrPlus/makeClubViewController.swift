d0ef082ec00a6915042b7d37780181d59ea89fe9
Felix Toft <felixtoft09@gmail.com>
2020-03-27 21:45:05 -0700

    New Club Creation Page

--- ClustrPlus/makeClubViewController.swift
+++ ClustrPlus/makeClubViewController.swift
@@ -11,11 +11,14 @@ import Firebase
 
 class makeClubViewController: UIViewController {
     var ref : DatabaseReference!
+    
+    
     @IBOutlet weak var clubNameField: UITextField!
     
-    @IBOutlet weak var signUpLinkField: UITextField!
     
     @IBOutlet weak var descriptionField: UITextView!
+    
+    @IBOutlet weak var linkField: UITextField!
     
     func showAlert(message : String){
         let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
@@ -28,12 +31,14 @@ class makeClubViewController: UIViewController {
     
     @IBAction func savePressed(_ sender: Any) {
         let clubName = clubNameField.text
-        let signUpLink = signUpLinkField.text
         let description = descriptionField.text
+        let signUpLink = linkField.text
         
         
-        if clubName != "" && signUpLink != "" && description != ""{
+        
+        if clubName != ""  && description != "" && signUpLink != "" {
             //implement
+            
         }else{
             showAlert(message: "Fill out the form please.")
         }
@@ -42,6 +47,12 @@ class makeClubViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         ref = Database.database().reference()
+        
+        let borderColor : UIColor = UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1.0)
+        descriptionField.layer.borderWidth = 1
+        descriptionField.layer.borderColor = borderColor.cgColor
+        descriptionField.layer.cornerRadius = 5.0
+       
     }
 
 }
